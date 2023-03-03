c7050329fcd89e514b3257dc68309d8bb901bbee
I.Muratov <ivanmuratov.2016@gmail.com>
2023-03-03 21:46:32 +0200

    secret_key_fixed

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-114888@7guat!ai__504q*%l!x(!6u#e_ut6tcl)r)llxe$y$t'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-114888@7guat!ai__504q*%l!x(!6u#e_ut6tcl)r)llxe$y$t')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
